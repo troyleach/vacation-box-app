7840ac356ffabb3ce55f162025b22731a5c0149c
Troy Leach <troyleach29@gmail.com>
2015-03-04 16:56:19 -0600

    backing up box wireframe

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
   
   devise_scope :user do
      authenticated :user do
-       root 'links#index', as: :authenticated_root
+       root 'vacations#index', as: :authenticated_root
      end
 
      unauthenticated do
        root 'devise/registrations#new', as: :unauthenticated_root
      end
    end
+
+   resources :vacations
 
 end

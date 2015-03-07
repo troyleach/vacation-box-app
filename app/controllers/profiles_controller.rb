86d74bf2c7b96fa9b9d237257b93f1ad96284de4
Troy Leach <troyleach29@gmail.com>
2015-03-07 13:37:06 -0600

    Added the form tags, picture

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,11 @@
+class ProfilesController < ApplicationController
+
+  def show
+    # write an if statement that if they want to add their own picture
+    # instead of the gravatar pic.
+  end
+
+  def update
+  end
+
+end

5d28eb792a74f054623623ebb953de1638053b69
Troy Leach <troyleach29@gmail.com>
2015-03-09 14:51:32 -0500

    Generated the vacation_accomodations controller

---
+++ app/controllers/vacation_accommodations_controller.rb
@@ -0,0 +1,2 @@
+class VacationAccommodationsController < ApplicationController
+end
---
+++ spec/controllers/vacation_accommodations_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe VacationAccommodationsController, :type => :controller do
+
+end

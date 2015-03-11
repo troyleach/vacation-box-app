8355c0275dcd32787ea5806bc4d40446eb3d601f
Troy Leach <troyleach29@gmail.com>
2015-03-11 16:13:23 -0500

    finished some styling of the two headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  @vacation  = Vacation.new
+  
+
   # helper_method :current_profile
 
   # def current_profile
@@ -25,9 +28,3 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :password, :password_confirmation, :current_password) }
     end
 end
-
-# def configure_permitted_parameters
-#   devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :city, :state) }
-#   devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
-#   devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password, :last_name) }
-# end
